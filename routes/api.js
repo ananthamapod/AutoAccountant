fe34c186b41c4261f16f9ab578841cb5c0d7fd92
Ananth Rao <ananthamapod@gmail.com>
2017-08-11 16:38:06 -0400

    Adding goal CRUD routes

--- routes/api.js
+++ routes/api.js
@@ -48,25 +48,110 @@ router.get('/transactions', (req, res, next) => {
 router.get('/accounts', (req, res, next) => {
   Account.find({})
     .then((accounts) => {
+      let newAccounts = accounts.map((a) => {
+        a.account_id = a._id
+        a.balances = {
+          available: a.available_balance,
+          current: a.current_balance,
+          limit: a.limit
+        }
+        debug(a.balances)
+        delete a.available_balance
+        delete a.current_balance
+        delete a.limit
+        delete a._id
+        return a
+      })
       res.json({
-        accounts: accounts.map((a) => {
-          a.account_id = a._id
-          a.balances = {
-            available: a.available_balance,
-            current: a.current_balance,
-            limit: a.limit
-          }
-          debug(a.balances)
-          delete a.available_balance
-          delete a.current_balance
-          delete a.limit
-          delete a._id
-          return a
-        })
+        accounts: newAccounts
       })
       debug(accounts)
     })
     .reject((err) => debug(err))
+})
+
+/* GET route for getting goals from database */
+router.get('/goals', (req, res, next) => {
+  Goal.find({})
+    .then((goals) => {
+      res.json({
+        goals: goals
+      })
+      debug(goals.length + ' goals retrieved')
+      debug(goals)
+    })
+    .reject((err) => debug(err))
+})
+
+/* POST route for creating new goals */
+router.post('/goals', (req, res, next) => {
+  debug(req.body)
+  Goal.create(req.body, (err, doc) => {
+    if (err) {
+      debug(err)
+      res.json(err)
+    } else {
+      debug(doc)
+      res.send("Goal added successfully!")
+    }
+  })
+})
+
+/* DELETE route for deleting goals by id */
+router.delete('/goals/:id', (req, res, next) => {
+  let goal_id = req.params.id
+  debug(`Deleting goal ${goal_id}`)
+  if (goal_id != "") {
+    Goal.remove({_id: goal_id}, (err) => {
+      if (err) {
+        res.json(err)
+      } else {
+        res.send(`Goal ${goal_id} deleted successfully!`)
+      }
+    })
+  }
+})
+
+/* GET route for getting bills from database */
+router.get('/bills', (req, res, next) => {
+  Bill.find({})
+    .then((bills) => {
+      res.json({
+        bills: bills
+      })
+      debug(bills.length + ' bills retrieved')
+      debug(bills)
+    })
+    .reject((err) => debug(err))
+})
+
+/* POST route for creating new bills */
+router.post('/bills', (req, res, next) => {
+  debug(req.body)
+  Bill.create(req.body, (err, doc) => {
+    if (err) {
+      debug(err)
+      res.json(err)
+    } else {
+      debug(doc)
+      res.send("Bill added successfully!")
+    }
+  })
+})
+
+/* DELETE route for deleting bills by id */
+router.delete('/bills/:id', (req, res, next) => {
+  let bill_id = req.params.id
+  debug(`Deleting bill ${bill_id}`)
+  if (bill_id != "") {
+    Bill.remove({_id: bill_id}, (err) => {
+      if (err) {
+        res.json(err)
+      } else {
+        res.send(`Bill ${bill_id} deleted successfully!`)
+      }
+    })
+  }
 })
 
 module.exports = router
