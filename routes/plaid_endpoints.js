98a7fffe20b5b0ee9d8cb6c1d328116da5bdf952
Ananth Rao <ananthamapod@gmail.com>
2017-09-12 10:36:08 -0400

    Add support for multiple account linking and promisifying Plaid client

--- routes/plaid_endpoints.js
+++ routes/plaid_endpoints.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const envvar = require('envvar')
+const Promise = require('bluebird')
 const express = require('express')
 const moment = require('moment')
 const plaid = require('plaid')
@@ -26,12 +27,12 @@ let PUBLIC_TOKEN = null
 let ITEM_ID = envvar.string('ITEM_ID')
 
 // Initialize the Plaid client
-const client = new plaid.Client(
+const client = Promise.promisifyAll(new plaid.Client(
   PLAID_CLIENT_ID,
   PLAID_SECRET,
   PLAID_PUBLIC_KEY,
   plaid.environments[PLAID_ENV]
-)
+))
 
 /* Utility function for mapping models */
 function applyForEach(iterable, func) {
@@ -89,14 +90,7 @@ router.get('/plaid/', passport.authenticate('jwt', { session: false }),
 /* POST route for getting access token and storing new items */
 router.post('/get_access_token', (req, res, next) => {
   PUBLIC_TOKEN = req.body.public_token
-  client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
-    if (error != null) {
-      var msg = 'Could not exchange public_token!'
-      debug(msg + '\n' + error)
-      return res.json({
-        error: msg
-      })
-    }
+  client.exchangePublicToken(PUBLIC_TOKEN).then(function(tokenResponse) {
     ACCESS_TOKEN = tokenResponse.access_token
     ITEM_ID = tokenResponse.item_id
     debug('Access Token: ' + ACCESS_TOKEN)
@@ -105,83 +99,136 @@ router.post('/get_access_token', (req, res, next) => {
       'error': false
     })
     saveItemInfo(ACCESS_TOKEN, ITEM_ID)
+  }).catch((error) => {
+    var msg = 'Could not exchange public_token!'
+    debug(msg + '\n' + JSON.stringify(error))
+    return res.json({
+      error: msg
+    })
   })
 })
 
-/* GET route for getting high level account information */
-router.get('/accounts', (req, res, next) => {
+/* POST route for getting high level account information and storing in local db */
+router.post('/accounts', (req, res, next) => {
   // Retrieve high-level account information and account and routing numbers
   // for each account associated with the Item.
-  client.getAuth(ACCESS_TOKEN, (error, authResponse) => {
-    if (error != null) {
-      let msg = 'Unable to pull accounts from the Plaid API.'
-      debug(msg + '\n' + error)
-      return res.json({
-        error: msg
-      })
-    }
-
-    debug(authResponse.accounts)
-    res.json({
-      error: false,
-      accounts: authResponse.accounts,
-      numbers: authResponse.numbers,
-    })
-
-    applyForEach(authResponse.accounts, addAccount)
-  })
-
-})
-
-/* POST route for getting Plaid item info */
-router.post('/item', (req, res, next) => {
-  // Pull the Item - this includes information about available products,
-  // billed products, webhook information, and more.
-  client.getItem(ACCESS_TOKEN, function(error, itemResponse) {
-    if (error != null) {
-      debug(JSON.stringify(error))
-      return res.json({
-        error: error
-      })
-    }
-
-    // Also pull information about the institution
-    client.getInstitutionById(itemResponse.item.institution_id, (err, instRes) => {
-      if (err != null) {
-        var msg = 'Unable to pull institution information from the Plaid API.'
-        debug(msg + '\n' + error)
+  Item.find({})
+  .then((items) => {
+    Promise.reduce(items, (accountList, item) => {
+      return client.getAuth(item.access_token).then((authResponse) => {
+        let accounts = accountList.accounts
+        let numbers = accountList.numbers
+        accounts.push.apply(accounts, authResponse.accounts)
+        numbers.push.apply(numbers, authResponse.numbers)
+        debug(accountList)
+        return accountList
+      }).catch((error) => {
+        let msg = 'Unable to pull accounts from the Plaid API.'
+        debug(msg + '\n' + JSON.stringify(error))
         return res.json({
           error: msg
         })
-      } else {
-        res.json({
-          item: itemResponse.item,
-          institution: instRes.institution,
-        })
-      }
+      })
+    }, {accounts: [], numbers: []}).then((items) => {
+      res.json({
+        error: false,
+        accounts: items.accounts,
+        numbers: items.numbers,
+      })
+      applyForEach(items.accounts, addAccount)
+      debug(items.accounts.length + ' items retrieved')
+      debug(items)
     })
+  })
+  .reject((err) => {
+    debug(err)
+    res.status(500).json({message:"Server error"})
   })
 })
 
-/* POST route for getting transactions */
+/* GET route for getting Plaid item info */
+router.get('/item', (req, res, next) => {
+  // Pull the Item - this includes information about available products,
+  // billed products, webhook information, and more.
+  // TODO: SLOPPY SLOPPY SLOPPY!
+  let counter = 0
+  let temp = ''
+  // TODO: END SLOPPY
+  Item.find({})
+  .then((items) => {
+    Promise.reduce(items, (itemList, item) => {
+      debug(`promise ${++counter} start`)
+      return client.getItem(item.access_token)
+      .then((itemResponse) => {
+        debug(`promise ${counter} then 1`)
+        // Also pull information about the institution
+        temp = itemResponse.item
+        return client.getInstitutionById(itemResponse.item.institution_id)
+      }).then((instRes) => {
+        debug(`promise ${counter} then 2`)
+        itemList.push({
+          item: temp,
+          institution: instRes.institution,
+        })
+        return itemList
+      }).catch((error) => {
+        debug(`promise ${counter} error`)
+        var msg = 'Unable to pull institution information from the Plaid API.'
+        debug(msg + '\n' + JSON.stringify(error))
+        return res.json({
+          error: msg
+        })
+      })
+    }, []).then((items) => {
+      res.json({
+        error: false,
+        items: items
+      })
+      debug(items.length + ' items retrieved')
+      debug(items)
+    })
+  })
+  .reject((err) => {
+    debug(err)
+    res.status(500).json({message:"Server error"})
+  })
+})
+
+/* POST route for getting transactions and storing in local db */
 router.post('/transactions', (req, res, next) => {
   // Pull transactions for the Item for the last 30 days
   let startDate = moment().subtract(2, 'years').format('YYYY-MM-DD')
   let endDate = moment().format('YYYY-MM-DD')
+  let counter = 0
   debug(startDate)
-  client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
-    offset: 0,
-  }, (error, transactionsResponse) => {
-    if (error != null) {
-      debug(JSON.stringify(error))
-      return res.json({
-        error: error
+  Item.find({})
+  .then((items) => {
+    Promise.reduce(items, (transactionList, item) => {
+      debug(`promise ${++counter} start`)
+      return client.getTransactions(item.access_token, startDate, endDate, {
+        offset: 0,
+      }).then((transactionsResponse) => {
+        debug(`promise ${counter} then`)
+        transactionList.push.apply(transactionList, transactionsResponse.transactions)
+        debug(transactionList)
+        return transactionList
+      }).catch((error) => {
+        let msg = 'Unable to pull transactions from the Plaid API.'
+        debug(msg + '\n' + JSON.stringify(error))
+        return res.json({
+          error: msg
+        })
       })
-    }
-    debug('pulled ' + transactionsResponse.transactions.length + ' transactions')
-    res.json({ transactions: transactionsResponse.transactions })
+    }, []).then((transactions) => {
+      debug('pulled ' + transactions.length + ' transactions')
+      res.json({ transactions: transactions })
 
-    applyForEach(transactionsResponse.transactions, addTransaction)
+      applyForEach(transactions, addTransaction)
+    })
+  })
+  .reject((err) => {
+    debug(err)
+    res.status(500).json({message:"Server error"})
   })
 })
 
@@ -218,6 +265,17 @@ router.get('/transactions/webhook', (req, res, next) => {
 
   }
   res.send("ACK")
+})
+
+router.get('/categories', (req, res, next) => {
+  client.getCategories().then((response) => {
+    res.json(response.categories)
+  }).catch((error) => {
+    debug(JSON.stringify(error))
+    return res.json({
+      error: error
+    })
+  })
 })
 
 module.exports = router
