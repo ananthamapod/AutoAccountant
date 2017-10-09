de37ab9126060520af6f77ef45866bdf90dee519
Ananth Rao <ananthamapod@gmail.com>
2017-10-09 18:13:05 -0400

    Adding some small charting, first steps to something more :)

--- src/js/components/expenses/Expenses.jsx
+++ src/js/components/expenses/Expenses.jsx
@@ -1,21 +1,49 @@
 // eslint-disable-next-line no-unused-vars
 import React, { Component } from 'react'
+// eslint-disable-next-line no-unused-vars
 import moment from 'moment'
 import { connect } from 'react-redux'
-import { Container } from 'reactstrap'
+import { Container, Row } from 'reactstrap'
+import { ResponsiveContainer, LineChart, XAxis, YAxis, CartesianGrid, Line } from 'recharts'
 
 class Expenses extends Component {
   constructor(props) {
     super(props)
   }
 
+  componentDidMount() {
+  }
+
   render() {
     return (
       <Container>
-        <h1 className="py-3">Expenses</h1>
+        <Row>
+          <h1 className="py-3">Expenses</h1>
+        </Row>
+        <Row style={{height: "800px"}}>
+          <ResponsiveContainer>
+            <LineChart data={this.props.transactions.items}>
+              <XAxis dataKey="name"/>
+              <YAxis/>
+              <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+              <Line type="monotone" dataKey="amount" stroke="#8884d8" />
+              // <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+            </LineChart>
+          </ResponsiveContainer>
+        </Row>
       </Container>
     )
   }
 }
 
-export default Expenses
+const mapStateToProps = (state) => {
+  return {
+    transactions: state.transactions
+  }
+}
+
+const ExpensesContainer = connect(
+  mapStateToProps
+)(Expenses)
+
+export default ExpensesContainer
